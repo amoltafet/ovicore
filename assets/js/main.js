6bf292a2260950da2f7d31dcf9700c2ce825683a
Ahmad Moltafet <moltafet.ahmad2015@gmail.com>
2022-08-13 23:01:27 -0700

    added meta data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: HeroBiz - v2.2.0
-* Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
